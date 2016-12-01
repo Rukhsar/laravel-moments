ad102190c1666711958c714a4fcde7a9ac7d3977
Rukhsar <rukhsar.man@gmail.com>
2016-12-01 12:01:50 +0000

    hours and days scopes added

--- src/Moments.php
+++ src/Moments.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 trait Moments
 {
     /**
-     * Scope a query to only include the current minute entries.
+     * Only include the current minute records from model.
      *
      * @param $query
      * @return \Illuminate\Database\Eloquent\Builder
@@ -18,6 +18,86 @@ trait Moments
             [
                 Carbon::now()->second(0),
                 Carbon::now()
+            ]
+        );
+    }
+
+    /**
+     * Only include the last minute records from model.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopelastMinute($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now()->subMinute()->second(0),
+                Carbon::now()->second(0)
+            ]
+        );
+    }
+
+    /**
+     * Only include the current hour records from model.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCurrentHour($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now()->minute(0)->second(0),
+                Carbon::now()
+            ]
+        );
+    }
+
+    /**
+     * Only include the last hour records from model.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeLastHour($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now()->subHour()->minute(0)->second(0),
+                Carbon::now()->minute(0)->second(0)
+            ]
+        );
+    }
+
+    /**
+     * Only include the current day records from model.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCurrentDay($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now()->startOfDay(),
+                Carbon::now()
+            ]
+        );
+    }
+
+    /**
+     * Only include the last day records from model.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeLastDay($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now()->subDay()->startOfDay(),
+                Carbon::now()->startOfDay()
             ]
         );
     }
