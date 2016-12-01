6d58bc33a81ea1bd76aacf8f3b3dac32df003134
Rukhsar <rukhsar.man@gmail.com>
2016-12-01 10:42:17 +0000

    getting record for current minute added

--- src/Moments.php
+++ src/Moments.php
@@ -1,1 +1,23 @@
 <?php
+
+namespace Rukhsar\Moments;
+
+use Carbon\Carbon;
+
+trait Moments;
+{
+    /**
+     * Scope a query to only include the current minute entries.
+     *
+     * @param $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeCurrentMinute($query)
+    {
+        return $query->whereBetween('created_at',
+            [
+                Carbon::now->second(0),
+                Carbon::now()
+            ]);
+    }
+}
